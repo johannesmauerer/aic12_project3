939241bc1e932ad6934541c1696133a9974c651b
haja <h.jagenteufel@gmail.com>
2012-10-31 18:01:13 +0100

    DI via @Autowire

--- src/main/java/aic12/service/requestManagement/RequestAnalysis.java
+++ src/main/java/aic12/service/requestManagement/RequestAnalysis.java
@@ -1,5 +1,15 @@
 package aic12.service.requestManagement;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class RequestAnalysis {
 
+	@Autowired
+	private RequestQueueReady requestQueueReady;
+
+	public RequestQueueReady getRequestQueueReady() {
+		return requestQueueReady;
+	}
 }
--- src/main/java/aic12/service/requestManagement/RequestQueueReady.java
+++ src/main/java/aic12/service/requestManagement/RequestQueueReady.java
@@ -1,5 +1,12 @@
 package aic12.service.requestManagement;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class RequestQueueReady {
+
+	public String some() {
+		return "some";
+	}
 
 }
