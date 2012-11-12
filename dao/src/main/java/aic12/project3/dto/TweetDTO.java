39a6ad71f357e79affc560a4df3f717fea125f87
lpuschmann <lukas.puschmann@gmx.at>
2012-11-12 16:40:59 +0100

    crated basic DTO's

--- dao/src/main/java/aic12/project3/dto/TweetDTO.java
+++ dao/src/main/java/aic12/project3/dto/TweetDTO.java
@@ -6,10 +6,12 @@ public class TweetDTO{
 
 	private String text;
 	private Date date;
+	private Integer sentiment;
 	
 	public TweetDTO(String text, Date date){
 		this.text = text;
 		this.date = date;
+		this.sentiment = null;
 	}
 
 	public String getText() {
@@ -28,8 +30,16 @@ public class TweetDTO{
 		this.date = date;
 	}
 	
+	public Integer getSentiment() {
+		return sentiment;
+	}
+
+	public void setSentiment(Integer sentiment) {
+		this.sentiment = sentiment;
+	}
+
 	@Override
 	public String toString() {
-		return "TweetDTO [text=" + text + ", date=" + date + "]";
+		return "TweetDTO [text=" + text + ", date=" + date + ", sentiment=" + (sentiment==null?sentiment:"null") +"]";
 	}
 }
