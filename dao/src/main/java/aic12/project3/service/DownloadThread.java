ad07108104699e5552cca56e515c8a6fdeacce74
haja <h.jagenteufel@gmail.com>
2012-11-23 20:56:11 +0100

    dlmanager: logging in DownloadThread (for initial downloading)

--- dao/src/main/java/aic12/project3/service/DownloadThread.java
+++ dao/src/main/java/aic12/project3/service/DownloadThread.java
@@ -2,6 +2,7 @@ package aic12.project3.service;
 
 import java.util.List;
 
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
@@ -20,6 +21,8 @@ public class DownloadThread extends Thread {
 	//@Autowired
 	private DownloadManagerService dlManagerService;
 	
+	private Logger log = Logger.getLogger(DownloadThread.class);
+	
 	public DownloadThread(SentimentRequest req) {
 		ApplicationContext ctx = new GenericXmlApplicationContext("app-config.xml");
 		tweetDao = ctx.getBean(ITweetDAO.class);
@@ -31,13 +34,16 @@ public class DownloadThread extends Thread {
 	@Override
 	public void run() {
 		// start download and save to db
+		log.info("starting initial download for " + request.getCompanyName());
 		List<TweetDTO> tweets = twitter.getAllTweets(request);
 		
 		// save to dao
+		log.debug("saving tweets from initial download for " + request.getCompanyName());
 		tweetDao.storeTweet(tweets);
 		
 		// notify DownloadManagerService when finished and terminate
 		dlManagerService.initialDownloadFinished(request, this);
+		log.info("finished initial download for " + request.getCompanyName());
 	}
 
 }
