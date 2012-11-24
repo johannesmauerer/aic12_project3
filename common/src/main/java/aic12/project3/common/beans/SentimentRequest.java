1534516f33cb8cd0553c026ebd64bee9765e8213
haja <h.jagenteufel@gmail.com>
2012-11-24 18:28:59 +0100

    common: SentimentRequest: add equals + hashCode on id

--- common/src/main/java/aic12/project3/common/beans/SentimentRequest.java
+++ common/src/main/java/aic12/project3/common/beans/SentimentRequest.java
@@ -9,7 +9,13 @@ public class SentimentRequest
     private Date from;
     private Date to;
 
-    public int getId()
+    public SentimentRequest() { }
+    
+    public SentimentRequest(int id) {
+    	this.id = id;
+	}
+
+	public int getId()
     {
         return id;
     }
@@ -52,4 +58,26 @@ public class SentimentRequest
     public String toString(){
     	return this.getCompanyName() + " - from: " + this.getFrom().toString() + " to: " + this.getTo().toString() + " with ID: " + this.getId(); 
     }
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		SentimentRequest other = (SentimentRequest) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
 }
