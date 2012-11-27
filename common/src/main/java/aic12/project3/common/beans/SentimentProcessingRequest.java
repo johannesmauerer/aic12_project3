91849289bbffa463438709300e54bdc3e6cc9fe9
johannesmauerer <johannes@mauerer.info>
2012-11-27 16:48:55 +0100

    Clean Statistics

---
+++ common/src/main/java/aic12/project3/common/beans/SentimentProcessingRequest.java
@@ -0,0 +1,12 @@
+package aic12.project3.common.beans;
+
+/**
+ * Sub-processing part of Sentiment Request
+ * @author johannes
+ *
+ */
+public class SentimentProcessingRequest {
+	
+	
+
+}
--- management/src/main/java/aic12/project3/service/statistics/StatisticsImpl.java
+++ management/src/main/java/aic12/project3/service/statistics/StatisticsImpl.java
@@ -1,12 +1,5 @@
 package aic12.project3.service.statistics;
 
-import java.util.Observable;
-import java.util.Observer;
-
-import org.springframework.beans.factory.annotation.Autowired;
-
-import aic12.project3.common.beans.SentimentRequest;
-import aic12.project3.service.requestManagement.RequestQueueReady;
 
 /**
  * 
@@ -14,63 +7,8 @@ import aic12.project3.service.requestManagement.RequestQueueReady;
  *
  */
 
-public class StatisticsImpl implements Statistics, Observer {
+public class StatisticsImpl implements Statistics {
 
-	@Autowired RequestQueueReady rqr;
-	private int queueSize;
-	
-	
-	private void updateStats(){
-		setQueueSize(rqr.getRequestQueueSize());
-	}
-	
-	public String toString(){
-		updateStats();
-		return "QueueSize: " + queueSize;
-		
-	}
 
-	/**
-	 * @return the queueSize
-	 */
-	public int getQueueSize() {
-		return queueSize;
-	}
-
-	/**
-	 * @param queueSize the queueSize to set
-	 */
-	public void setQueueSize(int queueSize) {
-		this.queueSize = queueSize;
-	}
-
-	/**
-	 * Delegate to implementation
-	 */
-	@Override
-	public void update(Observable arg0, Object arg1) {
-		this.updateInQueue((String) arg1);
-	}
-	
-	/**
-	 * Do all relevant statistics
-	 */
-	@Override
-	public void updateInQueue(String id){
-		// TODO
-		this.updateStats();
-	}
-
-	@Override
-	public void calculateProcessingTime(String id) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public SentimentRequest getRequestFromDB(String id) {
-		// TODO Auto-generated method stub
-		return null;
-	}
 	
 }
