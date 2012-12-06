a927aa0002486acc354d54e2c581acd45fe6019f
Martinka <Martinka@Martinka-PC>
2012-12-06 01:54:31 +0100

    structure slightly changed + confidence interval for response implemented

--- webserver/src/main/java/controller/LoginController.java
+++ webserver/src/main/java/controller/LoginController.java
@@ -37,85 +37,85 @@ public class LoginController {
 	}
 	
 	public String loginAction(){
-    	
-		/*
-		 * send request to requestService if company already reqistered
-		 */
-		setRequestService(new RequestService());
-		String response = requestService.findCompany(this.name);
-		
-		/*
-		 * if company not yet registered
-		 */
-		if(response==null){
-			/*
-			 * create user
-			 */
-			UserDTO user = new UserDTO();
-			user.setCompanyName(this.name);
-			System.out.println("USER: " + user.getCompanyName() + " created.");
-			requestService.insertCompany(user);
-			System.out.println("USER: " + user.getCompanyName() + " put into database.");
-			return "login";
-			
-		} 
-		/*
-		 * if company already registered
-		*/ 
-		else {
-			
-			
-			
-		/*SentimentRequestStats s1 = new SentimentRequestStats();
-		s1.setTweets(5);
-		s1.setSentiment(1.2f);
-		s1.setIntervalMin(1.0);
-		s1.setIntervalMax(2.0);
-		
-		SentimentRequestStats s2 = new SentimentRequestStats();
-		s2.setTweets(7);
-		s2.setSentiment(1.5f);
-		s2.setIntervalMin(1.3);
-		s2.setIntervalMax(2.8);
-		
-		SentimentRequestStats s3 = new SentimentRequestStats();
-		s3.setTweets(5);
-		s3.setSentiment(1.2f);
-		s3.setIntervalMin(1.0);
-		s3.setIntervalMax(2.0);
-		
-		requestStats.add(s1);
-		requestStats.add(s2);
-		requestStats.add(s3);*/
-		
-		System.out.println("USER: " + this.name + " - getting requests.");
-		SentimentRequestList listObject = requestService.getCompanyRequests(this.name);
-		System.out.println("USER: " + this.name + " - requests obtained.");
-		setPastRequest(listObject.getList());
-			
-		/*List<SentimentRequest> subRequests = pastRequests.getList();
-			
-			for(SentimentRequest subRequest : subRequests){
-				subRequest.getNumberOfTweets();
-				
-				
-			
-				double interval = 1.96 * Math.sqrt(response.getSentiment() * (1 - response.getSentiment()) / (response.getNumberOfTweets() - 1));
-				 
-				System.out.println("Amount: " + response.getNumberOfTweets() + " - Sentiment: (" + (response.getSentiment() - interval) + " < " + response.getSentiment() + " < "
-				                    + (response.getSentiment() + interval) + ")");
-				
-			}*/
-			
-			return "loggedIn";
-		}
-		
-		/*//TESTING
+//    	
+//		/*
+//		 * send request to requestService if company already reqistered
+//		 */
+//		setRequestService(new RequestService());
+//		String response = requestService.findCompany(this.name);
+//		
+//		/*
+//		 * if company not yet registered
+//		 */
+//		if(response==null){
+//			/*
+//			 * create user
+//			 */
+//			UserDTO user = new UserDTO();
+//			user.setCompanyName(this.name);
+//			System.out.println("USER: " + user.getCompanyName() + " created.");
+//			requestService.insertCompany(user);
+//			System.out.println("USER: " + user.getCompanyName() + " put into database.");
+//			return "login";
+//			
+//		} 
+//		/*
+//		 * if company already registered
+//		*/ 
+//		else {
+//			
+//			
+//			
+//		/*SentimentRequestStats s1 = new SentimentRequestStats();
+//		s1.setTweets(5);
+//		s1.setSentiment(1.2f);
+//		s1.setIntervalMin(1.0);
+//		s1.setIntervalMax(2.0);
+//		
+//		SentimentRequestStats s2 = new SentimentRequestStats();
+//		s2.setTweets(7);
+//		s2.setSentiment(1.5f);
+//		s2.setIntervalMin(1.3);
+//		s2.setIntervalMax(2.8);
+//		
+//		SentimentRequestStats s3 = new SentimentRequestStats();
+//		s3.setTweets(5);
+//		s3.setSentiment(1.2f);
+//		s3.setIntervalMin(1.0);
+//		s3.setIntervalMax(2.0);
+//		
+//		requestStats.add(s1);
+//		requestStats.add(s2);
+//		requestStats.add(s3);*/
+//		
+//		System.out.println("USER: " + this.name + " - getting requests.");
+//		SentimentRequestList listObject = requestService.getCompanyRequests(this.name);
+//		System.out.println("USER: " + this.name + " - requests obtained.");
+//		setPastRequest(listObject.getList());
+//			
+//		/*List<SentimentRequest> subRequests = pastRequests.getList();
+//			
+//			for(SentimentRequest subRequest : subRequests){
+//				subRequest.getNumberOfTweets();
+//				
+//				
+//			
+//				double interval = 1.96 * Math.sqrt(response.getSentiment() * (1 - response.getSentiment()) / (response.getNumberOfTweets() - 1));
+//				 
+//				System.out.println("Amount: " + response.getNumberOfTweets() + " - Sentiment: (" + (response.getSentiment() - interval) + " < " + response.getSentiment() + " < "
+//				                    + (response.getSentiment() + interval) + ")");
+//				
+//			}*/
+//			
+//			return "loggedIn";
+//		}
+//		
+		//TESTING
 		  if(this.name.equals("jana")){
 			return "loggedIn";
 		}else{
 			return "login";
-		}*/
+		}
 		
 		
 	}
--- webserver/src/main/java/controller/RequestController.java
+++ webserver/src/main/java/controller/RequestController.java
@@ -5,6 +5,7 @@ import java.util.Date;
 import java.util.List;
 import java.util.UUID;
 
+import javax.annotation.processing.ProcessingEnvironment;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 import javax.ws.rs.GET;
@@ -21,191 +22,189 @@ import aic12.project3.common.enums.REQUEST_QUEUE_STATE;
 @ManagedBean
 @RequestScoped
 public class RequestController {
-   
+
 	private UUID id;
 	private String companyName;
-    private Date from;
-    private Date to;
-    private SentimentRequest response;
-    private List<SentimentProcessingRequest> subResponse;
-    private StatisticsBean statistics;
-    private String test;
-    
-    
-    private RequestService requestService;
-    
-    public String getCompanyName()
-    {
-        return companyName;
-    }
-
-    public void setCompanyName(String companyName)
-    {
-        this.companyName = companyName;
-    }
-
-    public Date getFrom()
-    {
-        return from;
-    }
-
-    public void setFrom(Date from)
-    {
-        this.from = from;
-    }
-
-    public Date getTo()
-    {
-        return to;
-    }
-
-    public void setTo(Date to)
-    {
-        this.to = to;
-    }
-    
-    private UUID generateId(){
-    	return UUID.randomUUID();
-    };
-    
-    public String sendToAnalysis(){
-    	
-    	/*
-    	 * generate id
-    	 */
-    	UUID generatedId = generateId();
-    	/*
-    	 * store generated id as attribute of requestController
-    	 */
-    	this.id = generatedId;
-    	
-    	/*
-    	 * create request
-    	 */
-    	SentimentRequest request = new SentimentRequest();
-    	request.setId(generatedId.toString());
-    	request.setCompanyName(this.companyName);
-    	request.setFrom(this.from);
-    	request.setTo(this.to);
-    	request.setState(REQUEST_QUEUE_STATE.NEW);
-    	    	
-    	requestService = new RequestService();
-    	requestService.sendRequestToAnalysis(request);
-    	
-    	/*
-    	 * TESTING PURPOSES
-    	
-    	SentimentRequest req = new SentimentRequest();
-    	req.setNumberOfTweets(657000);
-    	
-    	SentimentProcessingRequest pr1 = new SentimentProcessingRequest();
-    	pr1.setNumberOfTweets(12);
-    	pr1.setTimestampStartOfAnalysis(new Long(466877413));
-    	pr1.setSentiment(0.56f);
-    	
-    	SentimentProcessingRequest pr2 = new SentimentProcessingRequest();
-    	pr2.setNumberOfTweets(15);
-    	pr2.setTimestampStartOfAnalysis(new Long(16488));
-    	pr2.setSentiment(0.9f);
-    	
-    	SentimentProcessingRequest pr3 = new SentimentProcessingRequest();
-    	pr3.setNumberOfTweets(9751);
-    	pr3.setTimestampStartOfAnalysis(new Long(789656));
-    	pr3.setSentiment(0.13f);
-    	
-    	List<SentimentProcessingRequest> subReqs = new ArrayList<SentimentProcessingRequest>();
-    	subReqs.add(pr1);
-    	subReqs.add(pr2);
-    	subReqs.add(pr3);
-    	 */
-    
-    	return "wait";
-    }
-			
-    
-    public String getAnalysisStatistics(){
-    	
-    	requestService = new RequestService();
-    	
-    	this.statistics = requestService.getStatistics(); 
-    	
-    	/*
-    	 * TEST
-    	 
-    	StatisticsBean test = new StatisticsBean();
-    	test.setAverageDurationPerRequest(4464);
-    	test.setAverageProcessingDurationPerTweet(4464);
-    	test.setAverageTotalDurationPerTweet(4);
-    	test.setMaximumDurationOfRequest(545646);
-    	test.setMinimumDurationOfRequest(98789749);
-    	this.statistics=test;*/
-    	
-    	return "statistics";
-    }
-    
-    public void getResponseFromDB(){
-    	
-    	requestService = new RequestService();
-    	
-    	this.response =requestService.getRequestResponse(this.id);
-    	    	
-    }
-
-
-	public String refresh(){
-		        
-		/*
-		SentimentProcessingRequest req = new SentimentProcessingRequest();
-    	req.setNumberOfTweets(657000);
-    	req.setSentiment(0.13f);
-    	req.setFrom(new Date());
-    	req.setTo(new Date());
-    	
-    	SentimentProcessingRequest pr1 = new SentimentProcessingRequest();
-    	pr1.setNumberOfTweets(12);
-    	pr1.setSentiment(0.56f);
-    	pr1.setFrom(new Date());
-    	pr1.setTo(new Date());
-    	
-    	SentimentProcessingRequest pr2 = new SentimentProcessingRequest();
-    	pr2.setNumberOfTweets(15);
-    	pr2.setSentiment(0.9f);
-    	pr2.setFrom(new Date());
-    	pr2.setTo(new Date());
-    	
-    	SentimentProcessingRequest pr3 = new SentimentProcessingRequest();
-    	pr3.setNumberOfTweets(9751);
-    	pr3.setSentiment(0.13f);
-    	pr3.setFrom(new Date());
-    	pr3.setTo(new Date());
-    	
-    	return "response";*/
-    	
-		getResponseFromDB();
-		if(this.response==null){
-			if(test!=null){
-				return "response";
-			}
-			return "wait";
-		}
-		else{
-			
-			this.subResponse=response.getSubRequests();
-			return "response";
-		}
-		
-	}
+	private Date from;
+	private Date to;
+	private int numberOfTweets;
+	private double minimumSentiment;
+	private double maximumSentiment;
+	private float sentiment;
 	
+	private StatisticsBean statistics;
+
+	private RequestService requestService;
+
+
+	private UUID generateId() {
+		return UUID.randomUUID();
+	};
+	
+	public String sendToAnalysis() {
+
+		/*
+		 * generate id
+		 */
+		UUID generatedId = generateId();
+		/*
+		 * store generated id as attribute of requestController
+		 */
+		this.id = generatedId;
+
+		/*
+		 * create request
+		 */
+		SentimentRequest request = new SentimentRequest();
+		request.setId(generatedId.toString());
+		request.setCompanyName(this.companyName);
+		request.setFrom(this.from);
+		request.setTo(this.to);
+		request.setState(REQUEST_QUEUE_STATE.NEW);
+
+		requestService = new RequestService();
+		// requestService.sendRequestToAnalysis(request); TODO
+
+		/*
+		 * TESTING PURPOSES
+		 * 
+		 * SentimentRequest req = new SentimentRequest();
+		 * req.setNumberOfTweets(657000);
+		 * 
+		 * SentimentProcessingRequest pr1 = new SentimentProcessingRequest();
+		 * pr1.setNumberOfTweets(12); pr1.setTimestampStartOfAnalysis(new
+		 * Long(466877413)); pr1.setSentiment(0.56f);
+		 * 
+		 * SentimentProcessingRequest pr2 = new SentimentProcessingRequest();
+		 * pr2.setNumberOfTweets(15); pr2.setTimestampStartOfAnalysis(new
+		 * Long(16488)); pr2.setSentiment(0.9f);
+		 * 
+		 * SentimentProcessingRequest pr3 = new SentimentProcessingRequest();
+		 * pr3.setNumberOfTweets(9751); pr3.setTimestampStartOfAnalysis(new
+		 * Long(789656)); pr3.setSentiment(0.13f);
+		 * 
+		 * List<SentimentProcessingRequest> subReqs = new
+		 * ArrayList<SentimentProcessingRequest>(); subReqs.add(pr1);
+		 * subReqs.add(pr2); subReqs.add(pr3);
+		 */
+
+		getResponseFromDB();
+		
+		return "response";
+	}
+
+	public String getAnalysisStatistics() {
+
+		requestService = new RequestService();
+
+		this.statistics = requestService.getStatistics();
+
+		/*
+		 * TEST
+		 * 
+		 * StatisticsBean test = new StatisticsBean();
+		 * test.setAverageDurationPerRequest(4464);
+		 * test.setAverageProcessingDurationPerTweet(4464);
+		 * test.setAverageTotalDurationPerTweet(4);
+		 * test.setMaximumDurationOfRequest(545646);
+		 * test.setMinimumDurationOfRequest(98789749); this.statistics=test;
+		 */
+
+		return "statistics";
+	}
+
+	public void getResponseFromDB() {
+
+		requestService = new RequestService();
+
+		/*
+		 * Test
+		 */
+		SentimentProcessingRequest req = new SentimentProcessingRequest();
+		req.setNumberOfTweets(657000);
+		req.setSentiment(0.13f);
+		req.setFrom(new Date());
+		req.setTo(new Date());
+
+		SentimentProcessingRequest pr1 = new SentimentProcessingRequest();
+		pr1.setNumberOfTweets(12);
+		pr1.setSentiment(0.56f);
+		pr1.setFrom(new Date());
+		pr1.setTo(new Date());
+
+		SentimentProcessingRequest pr2 = new SentimentProcessingRequest();
+		pr2.setNumberOfTweets(15);
+		pr2.setSentiment(0.9f);
+		pr2.setFrom(new Date());
+		pr2.setTo(new Date());
+
+		SentimentProcessingRequest pr3 = new SentimentProcessingRequest();
+		pr3.setNumberOfTweets(9751);
+		pr3.setSentiment(0.13f);
+		pr3.setFrom(new Date());
+		pr3.setTo(new Date());
+
+		List<SentimentProcessingRequest> subs = new ArrayList<SentimentProcessingRequest>();
+		subs.add(req);
+		subs.add(pr3);
+		subs.add(pr2);
+		subs.add(pr1);
+
+		SentimentRequest response = new SentimentRequest();
+		response.setSubRequests(subs);
+		/*
+		 * End test
+		 */
+
+		// SentimentRequest response =
+		// requestService.getRequestResponse(this.id);
+
+		float sumSentiment = 0;
+		int finalNumberOfTweets = 0;
+		int numberOfSubrequests = response.getSubRequests().size();
+
+		/*
+		 * calculating request details
+		 */
+		for (SentimentProcessingRequest subrequest : response.getSubRequests()) {
+
+			sumSentiment += subrequest.getSentiment();
+			finalNumberOfTweets += subrequest.getNumberOfTweets();
+
+		}
+
+		float finalSentiment = sumSentiment / numberOfSubrequests;
+
+		double standardError = 1.96 * Math.sqrt(finalSentiment
+				* (1 - finalSentiment) / (finalNumberOfTweets - 1));
+
+		this.sentiment = finalSentiment;
+		
+		this.numberOfTweets = finalNumberOfTweets;
+
+		this.minimumSentiment = finalSentiment - standardError;
+		this.maximumSentiment = finalSentiment + standardError;
+
+		System.out.println("Amount: " + numberOfTweets + " - Sentiment: ("
+				+ minimumSentiment + " < " + finalSentiment + " < "
+				+ maximumSentiment + ")");
+	}
+
+	/*
+	 * public String refresh(){
+	 * 
+	 * 
+	 * 
+	 * getResponseFromDB(); if(this.response==null){ if(test!=null){ return
+	 * "response"; } return "wait"; } else{
+	 * 
+	 * this.subResponse=response.getSubRequests(); return "response"; }
+	 * 
+	 * }
+	 */
+
 	public UUID getId() {
 		return id;
-	}
-
-	public SentimentRequest getResponse() {
-		return response;
-	}
-
-	public void setResponse(SentimentRequest response) {
-		this.response = response;
 	}
 
 	public StatisticsBean getStatistics() {
@@ -216,19 +215,72 @@ public class RequestController {
 		this.statistics = statistics;
 	}
 
-	public String getTest() {
-		return test;
+	public int getNumberOfTweets() {
+		return numberOfTweets;
 	}
 
-	public void setTest(String test) {
-		this.test = test;
+	public void setNumberOfTweets(int numberOfTweets) {
+		this.numberOfTweets = numberOfTweets;
 	}
 
-	public List<SentimentProcessingRequest> getSubResponse() {
-		return subResponse;
+	public double getMinimumSentiment() {
+		return minimumSentiment;
 	}
 
-	public void setSubResponse(List<SentimentProcessingRequest> subResponse) {
-		this.subResponse = subResponse;
+	public void setMinimumSentiment(double minimumSentiment) {
+		this.minimumSentiment = minimumSentiment;
 	}
+
+	public double getMaximumSentiment() {
+		return maximumSentiment;
+	}
+
+	public void setMaximumSentiment(double maximumSentiment) {
+		this.maximumSentiment = maximumSentiment;
+	}
+
+	public RequestService getRequestService() {
+		return requestService;
+	}
+
+	public void setRequestService(RequestService requestService) {
+		this.requestService = requestService;
+	}
+
+	public void setId(UUID id) {
+		this.id = id;
+	}
+
+	public String getCompanyName() {
+		return companyName;
+	}
+
+	public void setCompanyName(String companyName) {
+		this.companyName = companyName;
+	}
+
+	public Date getFrom() {
+		return from;
+	}
+
+	public void setFrom(Date from) {
+		this.from = from;
+	}
+
+	public Date getTo() {
+		return to;
+	}
+
+	public void setTo(Date to) {
+		this.to = to;
+	}
+
+	public float getSentiment() {
+		return sentiment;
+	}
+
+	public void setSentiment(float sentiment) {
+		this.sentiment = sentiment;
+	}
+
 }
